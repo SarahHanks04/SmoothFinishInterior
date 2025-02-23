33ff1d49e57d1dbcb3d153ef149bc127b88c3d77
SarahHanks04 <sarahhanks04@gmail.com>
2025-02-23 21:22:10 +0100

    Add Tailwind CSS configuration and Navbar component updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import NavbarImage from "../assets/navbar.jpg";
+import NavbarImage from "../assets/NavbarImage.png";
 
 const Navbar = () => {
   return (
-    <main>
-      <div>
-        <img src={NavbarImage} alt="NavbarImage" />
+    <header className="w-full bg-white shadow-md overflow-hidden">
+      <div className="w-full px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+        <img
+          src={NavbarImage}
+          alt="NavbarImage"
+          className="h-auto max-w-full md:max-w-[200px] lg:max-w-[250px]"
+        />
       </div>
-    </main>
+    </header>
   );
 };
 
