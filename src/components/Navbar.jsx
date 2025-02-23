da16a9cdcc6cef4e0ccbb17c21cde486defeb582
SarahHanks04 <sarahhanks04@gmail.com>
2025-02-23 20:55:35 +0100

    Add Navbar component and replace Navbar image

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarImage from "../assets/navbar.jpg";
+
+const Navbar = () => {
+  return (
+    <main>
+      <div>
+        <img src={NavbarImage} alt="NavbarImage" />
+      </div>
+    </main>
+  );
+};
+
+export default Navbar;
