0e53ed60634fbf26778d65da9bf7b190b3d2c55b
SarahHanks04 <sarahhanks04@gmail.com>
2025-02-27 00:44:41 +0100

    Refactor Navbar component: remove unused code and replace NavbarImage with Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,3 @@
-// import React from "react";
-// import NavbarImage from "../assets/NavbarImage.png";
-
-// const Navbar = () => {
-//   return (
-//     <header className="w-full bg-white shadow-md overflow-hidden">
-//       <div className="w-full px-4 sm:px-6 lg:px-8 flex items-center justify-between">
-//         <img
-//           src={NavbarImage}
-//           alt="NavbarImage"
-//           className="h-auto max-w-full md:max-w-[200px] lg:max-w-[250px]"
-//         />
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useState } from "react";
 import Logo from "../assets/Logo.png";
 
