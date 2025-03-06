50e77114f3d0d914e8969c6521817be85c9b6819
SarahHanks04 <sarahhanks04@gmail.com>
2025-03-06 22:15:38 +0100

    Implement user registration with Firebase Auth and update input handling

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import { auth } from "../config/firebase";
+import {createUserWithEmailAndPassword} from "firebase/auth";
+
+
 export const Auth = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // console.log(auth?.currentUser?.email)
+
+    const SignIn = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(user);
+        }
+        catch (error) {
+            console.error(error);
+        }   
+    }
   return (
     <div>
-      <input type="text" placeholder="Email...." />
-      <input type="text" placeholder="Password...." />
-        <button>SignIn</button>
+      <input type="email" placeholder="Email...." onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password...." onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={SignIn}>SignIn</button>
     </div>
   )
 }
