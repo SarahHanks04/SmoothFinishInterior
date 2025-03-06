2c1bd0491f64c5e182e8d44d53a3bcf508358397
SarahHanks04 <sarahhanks04@gmail.com>
2025-03-06 21:49:47 +0100

    Add authentication component and integrate Firebase Auth

---
+++ src/components/auth.jsx
@@ -0,0 +1,9 @@
+export const Auth = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Email...." />
+      <input type="text" placeholder="Password...." />
+        <button>SignIn</button>
+    </div>
+  )
+}
